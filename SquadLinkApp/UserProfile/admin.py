b45fa28a03b6832c32f6ad4681961c33f96fccfc
Christian Sarabia <csfromcs@gmail.com>
2022-11-22 15:33:12 +0800

    fix account friends and add user to admin view

--- SquadLinkApp/UserProfile/admin.py
+++ SquadLinkApp/UserProfile/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    model = SquadLinkUserModel
+    # list_display = [field.name for field in SquadLinkLobby._meta.get_fields()]
+    list_display = ('user', 'id', 'user_id', 'game')
+
+    def user_id(self, obj):
+        return obj.user.id
+
+
+admin.site.register(SquadLinkUserModel, UserProfileAdmin)
